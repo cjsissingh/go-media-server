187e728510a100f2a056e8ab0211b4afa18cdf9c
Chris Sissingh <chris@sissingh.me>
2023-08-20 20:02:37 -0700

    Move RGB conversion into Utils - not sure this is best practice

--- main.go
+++ main.go
@@ -32,31 +32,6 @@ func ping(c *gin.Context) {
 	})
 }
 
-type Hex string
-
-type RGB struct {
-	Red   uint8
-	Green uint8
-	Blue  uint8
-}
-
-func Hex2RGB(hex Hex) (RGB, error) {
-	var rgb RGB
-	values, err := strconv.ParseUint(string(hex), 16, 32)
-
-	if err != nil {
-		return RGB{}, err
-	}
-
-	rgb = RGB{
-		Red:   uint8(values >> 16),
-		Green: uint8((values >> 8) & 0xFF),
-		Blue:  uint8(values & 0xFF),
-	}
-
-	return rgb, nil
-}
-
 func getImage(c *gin.Context) {
 	md5AndTransforms := c.Params.ByName("md5AndTransforms")
 	if md5AndTransforms == "" {
